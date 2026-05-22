2d9f94e24c7b58a7f5c99be44724eb3352c4e6d3
Nicko Fajardo <nickofajardo97@gmail.com>
2026-05-22 14:56:18 +0800

    Feat: Dashboard 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+
 function App() {
   return (
     <div className="min-h-screen bg-blue-500 flex items-center justify-center">
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,33 @@
+import ResumeStrip from "../components/dashboard/ResumeStrip";
+import StatCard from "../components/dashboard/StatCard";
+import SubjectCard from "../components/dashboard/SubjectCard";
+
+export default function Dashboard() {
+    return (
+        <>
+
+        <div className="min-h-screen bg-gray-50">
+            <div className="max-w-2xl mx-auto px-5 py-8">
+                {/* Header Part */}
+                <div className="flex items-center justify-between mb-7">
+                    <div>
+                        <p className="text-sm text-gray-400 mb-0.5"> Good Morning!</p>
+                    </div>
+                    <h1 className="text-2xl font-semibold text-gray-900 tracking-tight">Entrance Exam Reviewer</h1>
+                </div>
+                <div className="w-9 rounded-full bg-violet-100 flex items-center justify-center text-sm font-semibold text-violet-700 select-none">
+                    NF
+                </div>
+            </div>
+
+            {/* StatCards */}
+
+        </div>
+        <ResumeStrip />
+        <StatCard />
+        <SubjectCard />
+        </> 
+ 
+    )
+}
+
